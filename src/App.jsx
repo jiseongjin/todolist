c21721937825f7d101f07a9763c7aa75b315d24a
Your Name <you@your-domain.com>
2024-01-18 21:57:05 +0900

    뼈대완성

---
+++ src/App.jsx
@@ -0,0 +1,37 @@
+import "./App.css";
+
+function App() {
+
+
+
+
+  
+  return (
+    <div>
+      <div>My Todo List</div>
+      <div>
+        <div>
+          제목 <input />
+        </div>
+        <div>
+          내용 <input />
+        </div>
+        <button>추가하기</button>
+      </div>
+      <div>
+        <div>Working..</div>
+        <div>
+          <div>리액트 공부하기</div>
+          <div>이렉트 기초를 공부해 봅시다.</div>
+          <button>삭제하기</button>
+          <button>완료</button>
+        </div>
+        <div>
+          <div>Done..!</div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
