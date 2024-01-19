b8fdffad900702b060586a7e2f45d39bcf5b6e7e
Your Name <you@your-domain.com>
2024-01-19 12:11:01 +0900

    css완료

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,40 @@
+import { useState } from "react";
 import "./App.css";
 
 function App() {
+  const [title, setTitle] = useState(""); 
+
+  const onChangeTitle = (event) =>{
+    const inputValue = event.target.value;
+    setTitle(inputValue)
+    console.log(title);
+  }
 
 
-
-
-  
   return (
-    <div>
-      <div>My Todo List</div>
-      <div>
-        <div>
-          제목 <input />
+    <div className="totd-list">
+      <div className="title-name">My Todo List</div>
+      <div className="plus-card">
+        <div className="card-name">
+          제목 <input className="input" type="text" onChange={onChangeTitle} value={title}/>
         </div>
-        <div>
-          내용 <input />
+        <div className="card-name">
+          내용 <input className="input"/>
         </div>
-        <button>추가하기</button>
+        <div><button className="plus-btn">추가하기</button></div>
       </div>
       <div>
-        <div>Working..</div>
-        <div>
-          <div>리액트 공부하기</div>
-          <div>이렉트 기초를 공부해 봅시다.</div>
-          <button>삭제하기</button>
-          <button>완료</button>
+        <div className="card-list-name">Working..</div>
+        <div className="cards">
+          <div className="card-main">리액트 공부하기</div>
+          <div className="card-detail">리액트 기초를 공부해 봅시다.</div>
+          <div>
+            <button className="delete-btn">삭제하기</button>
+            <button className="complete-delete-btn">완료</button>
+          </div>
         </div>
         <div>
-          <div>Done..!</div>
+          <div className="card-list-name">Done..!</div>
         </div>
       </div>
     </div>
