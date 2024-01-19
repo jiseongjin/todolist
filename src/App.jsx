d75ca5def6f161ed1e0670cc4c441fc320c49cd5
Your Name <you@your-domain.com>
2024-01-19 12:22:10 +0900

    제목,내용useState만듬

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,19 @@ import { useState } from "react";
 import "./App.css";
 
 function App() {
-  const [title, setTitle] = useState(""); 
+  const [title, setTitle] = useState("리액트");
+  const [detail, setDetail] = useState("리액트 기초를 공부");
 
-  const onChangeTitle = (event) =>{
+  const onChangeTitle = (event)=>{
     const inputValue = event.target.value;
-    setTitle(inputValue)
+    setTitle(inputValue);
     console.log(title);
   }
 
+  const onChangeDetail = (event)=>{
+    const inputValue = event.target.value;
+    setDetail(inputValue);
+  }
 
   return (
     <div className="totd-list">
@@ -19,15 +24,15 @@ function App() {
           제목 <input className="input" type="text" onChange={onChangeTitle} value={title}/>
         </div>
         <div className="card-name">
-          내용 <input className="input"/>
+          내용 <input className="input"type="text" onChange={onChangeDetail} value={detail}/>
         </div>
         <div><button className="plus-btn">추가하기</button></div>
       </div>
       <div>
         <div className="card-list-name">Working..</div>
         <div className="cards">
-          <div className="card-main">리액트 공부하기</div>
-          <div className="card-detail">리액트 기초를 공부해 봅시다.</div>
+          <div className="card-title">{title}</div>
+          <div className="card-detail">{detail}</div>
           <div>
             <button className="delete-btn">삭제하기</button>
             <button className="complete-delete-btn">완료</button>
