2615e90d2c40417405ebfb73f8444e94dab05f10
Your Name <you@your-domain.com>
2024-02-06 17:49:43 +0900

    개인과제

--- src/pages/Detail.jsx
+++ src/pages/Detail.jsx
@@ -1,10 +1,36 @@
 import React from "react";
 import { useParams } from "react-router";
+import {
+  CardDate,
+  CardDetail,
+  CardTitle,
+  CardsStyle,
+} from "../components/Styled";
 
-function Detail() {
+function Detail({ cards }) {
+  const options = {
+    weekday: "long",
+    year: "numeric",
+    month: "long",
+    day: "numeric",
+  };
   const params = useParams();
   console.log(params);
-  return <div>Detail</div>;
+  return (
+    <div>
+      {cards.map((item) => {
+        return item.id === params.id ? (
+          <CardsStyle key={item.id}>
+            <CardTitle>{item.title}</CardTitle>
+            <CardDetail>{item.detail}</CardDetail>
+            <CardDate isdone={item.isdone}>
+              {new Date(item.date).toLocaleString("ko-KR", options)}까지!
+            </CardDate>
+          </CardsStyle>
+        ) : null;
+      })}
+    </div>
+  );
 }
 
 export default Detail;
